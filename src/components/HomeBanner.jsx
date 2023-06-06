f836483af7bd56c76d6de19743cc798c125baae0
abhkrs <98683807+abhkrs@users.noreply.github.com>
2023-06-07 01:58:31 +0530

    Update HomeBanner.jsx

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -15,8 +15,7 @@ export default function HomeBanner() {
           />
           <div className="absolute bg-[#00000080] z-[2] top-0 bottom-0 left-0 right-0"/>
           <div className="h-12 absolute bottom-0 left-0 right-0 bg-transparent z-20">
-              <div className="h-4 bg-gradient-to-t from-black via-[#00000050] to-transparent" /> 
-              <div className="h-4 bg-primary" /> 
+              <div className="h-8 bg-gradient-to-t from-primary via-black to-transparent" /> 
               <div className="h-4 bg-gradient-to-b from-primary to-white" /> 
           </div>
           <div className="text-white text-center absolute z-[5] bottom-12  left-1/2 -translate-x-1/2 w-4/5">
