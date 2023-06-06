4d5a96b6b371714458485244b2ff5ef80cc6f5ff
abhkrs <98683807+abhkrs@users.noreply.github.com>
2023-06-07 01:38:17 +0530

    Update HomeBanner.jsx

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -14,11 +14,10 @@ export default function HomeBanner() {
           className="object-cover absolute top-0 bottom-0 z-[1]"
           />
           <div className="absolute bg-[#00000080] z-[2] top-0 bottom-0 left-0 right-0"/>
-          <div className="h-12 absolute bottom-0 left-0 right-0 bg-primary z-20">
-              <div className="h-2 relative top-0 left-0 right-0 bg-white" /> 
-              <div className="h-4 relative top-3 left-0 right-0 bg-white" /> 
+          <div className="h-6 absolute bottom-0 left-0 right-0 bg-primary z-20">
+              <div className="h-4 relative top-0 left-0 right-0 bg-gradient-to-t from-primary to-white z-20" /> 
           </div>
-          <div className="text-white text-center absolute z-[5] bottom-20  left-1/2 -translate-x-1/2 w-4/5">
+          <div className="text-white text-center absolute z-[5] bottom-12  left-1/2 -translate-x-1/2 w-4/5">
               <H2 className="tsad font-bold">Lorem ipsum dolor sit amet.</H2>
               <H2 className="tshad">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, ipsa.</H2>
               <div className="pt-6 pb-20 flex w-full relative z-50 lg:w-4/5 mx-auto">
