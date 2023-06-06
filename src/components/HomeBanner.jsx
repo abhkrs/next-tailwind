b29b220e515f5f0548648169ec095cb897e9d671
abhkrs <98683807+abhkrs@users.noreply.github.com>
2023-06-07 02:09:55 +0530

    Update HomeBanner.jsx

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -7,8 +7,8 @@ import H1 from './typography/H1'
 export default function HomeBanner() {
   return (
     <>
-      <section className="h-96">
-          <div className='h-96 overflow-hidden mb-16'>
+      <section>
+          <div className='h-screen overflow-hidden mb-16'>
             <Image
             fill
             src = '/images/sliderImage.png'
@@ -20,17 +20,16 @@ export default function HomeBanner() {
                 <div className="h-4 bg-primary" /> 
                 <div className="h-4 bg-gradient-to-b from-primary to-white" /> 
             </div>
-            <div className="text-white text-center absolute z-[5] bottom-12  left-1/2 -translate-x-1/2 w-4/5">
+            <div className="text-white text-center absolute z-[5] bottom-20  left-1/2 -translate-x-1/2 w-4/5">
                 <H2 className="tsad font-bold">Lorem ipsum dolor sit amet.</H2>
                 <H2 className="tshad">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, ipsa.</H2>
-                <div className="pt-6 pb-12 flex w-full relative z-50 lg:w-4/5 mx-auto">
+                <div className="pt-6 pb-20 flex w-full relative z-50 lg:w-4/5 mx-auto">
                     <input type="text" name="Search" id="search" className='rounded-l h-8 md:h-12 border-2 border-gray-200 shadow w-full min-w-[170px]'/>
                     <button className='bg-primary px-6 py-1 md:py-3 rounded-r'>Search</button>
                 </div>
             </div>
           </div>
       </section>
-      <div className="h-20 mb-20" />
     </>
   )
 }
