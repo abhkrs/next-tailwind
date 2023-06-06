7e42496804284cfa06e0eb1d150e376e18e36f87
abhkrs <98683807+abhkrs@users.noreply.github.com>
2023-06-07 01:48:45 +0530

    Update HomeBanner.jsx

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -14,18 +14,20 @@ export default function HomeBanner() {
           className="object-cover absolute top-0 bottom-0 z-[1]"
           />
           <div className="absolute bg-[#00000080] z-[2] top-0 bottom-0 left-0 right-0"/>
-          <div className="h-6 absolute bottom-0 left-0 right-0 bg-primary z-20">
-              <div className="h-4 relative top-2 left-0 right-0 bg-gradient-to-b from-primary to-white z-20" /> 
+          <div className="h-12 absolute bottom-0 left-0 right-0 bg-primary z-20">
+              <div className="h-4 relative top-0 left-0 right-0 bg-gradient-to-t from-black via-[#00000050] to-transparent z-20" /> 
+              <div className="h-4 relative bottom-0 left-0 right-0 bg-gradient-to-b from-primary to-white z-20" /> 
           </div>
           <div className="text-white text-center absolute z-[5] bottom-12  left-1/2 -translate-x-1/2 w-4/5">
               <H2 className="tsad font-bold">Lorem ipsum dolor sit amet.</H2>
               <H2 className="tshad">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, ipsa.</H2>
-              <div className="pt-6 pb-20 flex w-full relative z-50 lg:w-4/5 mx-auto">
-                  <input type="text" name="Search" id="search" className='rounded-l h-8 md:h-12 border-2 border-gray-200 shadow w-full min-w-[200px]'/>
+              <div className="pt-6 pb-12 flex w-full relative z-50 lg:w-4/5 mx-auto">
+                  <input type="text" name="Search" id="search" className='rounded-l h-8 md:h-12 border-2 border-gray-200 shadow w-full min-w-[170px]'/>
                   <button className='bg-primary px-6 py-1 md:py-3 rounded-r'>Search</button>
               </div>
           </div>
         </div>
     </section>
+    <div className="h-20" />
   )
 }
